fbd7feff8c52aec8fcbd201ee3a0aed8f66ebcfb
Akash Doloi <imdoloiakash@gmail.com>
2024-04-19 21:25:17 +0530

    create login & registration page

---
+++ client/src/pages/login/Login.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './login.css'
+import { Link } from 'react-router-dom'
+const Login = () => {
+  return (
+    <div className='login'>
+        <div className="card">
+            <h1>Login</h1>
+            <form>
+                <input type="text" placeholder='Username' />
+                <input type="password" placeholder='Password' />
+                <button>Login</button>
+            </form>
+            <p>Don't have an account? 
+                <Link to='/register'>Register</Link>
+                </p>
+        </div>
+    </div>
+  )
+}
+
+export default Login
